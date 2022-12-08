3a33c751bb8dca722d56329f3a6565bcb050ce45
murdoch2439 <murdochkhallz@gmail.com>
2022-12-08 08:26:43 +0100

    adding other categories in expense type

--- src/utils/expanseType.ts
+++ src/utils/expanseType.ts
@@ -1,6 +1,8 @@
 const expanseType = [
     {value:"errands", label:"Errands"},
-    {value:"goods", label:"Goods"}
+    {value:"purchase_goods", label:"Purchase Goods"},
+    {value:"daily_food", label:"Daily Food"},
+    {value:"transportation", label:"Transportation"},
 ]
 
 export {
