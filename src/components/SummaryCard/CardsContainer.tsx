97792a72659f838d9e714af086538b3b2d9c3299
mr2k <murdochkhallz@gmail.com>
2023-05-10 18:12:33 +0100

    Refactoring

--- src/components/SummaryCard/CardsContainer.tsx
+++ src/components/SummaryCard/CardsContainer.tsx
@@ -6,11 +6,37 @@ import {useUserContext} from "../../context/UserContext";
 
 export const CardsContainer: FunctionComponent =()=>{
     const {information:{statistics}} =useUserContext()
+    const cards = [
+        {
+            heading: "Total in",
+            title: "USD",
+            content: `${statistics ?statistics.total_balance_usd.toFixed(2):""}`,
+        },
+        {
+            heading: "Total in",
+            title: "CDF",
+            content: `${statistics ?statistics.total_balance_cdf.toFixed(2):""}`,
+        },
+        {
+            icon: <HiOutlineUserGroup className={"card__icon"}/>,
+            heading: "Members",
+            title: "Posts",
+            content: "10"
+        },
+
+    ]
     return(
         <div className={"cardsContainer"}>
-            <SummaryCard heading={"Total in"} title={"USD"} content={statistics ?statistics.total_balance_usd.toFixed(2):""} />
-            <SummaryCard heading={"Total in"} title={"CDF"} content={statistics ? statistics.total_balance_cdf.toLocaleString():""} />
-            <SummaryCard heading={"Members"} icon={<HiOutlineUserGroup className={"card__icon"} />} content={"10"} />
+            {
+                cards.map((card, index)=>{
+                    return(
+                        <SummaryCard key={index} {...card}/>
+                    )
+                })
+            }
+            {/*<SummaryCard heading={"Total in"} title={"USD"} content={statistics ?statistics.total_balance_usd.toFixed(2):""} />*/}
+            {/*<SummaryCard heading={"Total in"} title={"CDF"} content={statistics ? statistics.total_balance_cdf.toLocaleString():""} />*/}
+            {/*<SummaryCard heading={"Members"} icon={<HiOutlineUserGroup className={"card__icon"} />} content={"10"} />*/}
         </div>
 
     )
