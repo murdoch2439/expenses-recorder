0e3ddd65fe25150017b78fbb719071a9c3b14656
mr2k <murdochkhallz@gmail.com>
2023-05-10 03:15:25 +0100

    Adding data table

--- src/components/SummaryCard/CardsContainer.tsx
+++ src/components/SummaryCard/CardsContainer.tsx
@@ -2,12 +2,14 @@ import React, {FunctionComponent} from 'react';
 import {SummaryCard} from "./SummaryCard";
 import {HiOutlineUserGroup} from "react-icons/hi";
 import "./cardsStyle.css"
+import {useUserContext} from "../../context/UserContext";
 
 export const CardsContainer: FunctionComponent =()=>{
+    const {information:{statistics}} =useUserContext()
     return(
         <div className={"cardsContainer"}>
-            <SummaryCard heading={"Total in"} title={"USD"} content={"600.00"} />
-            <SummaryCard heading={"Total in"} title={"CDF"} content={"42,000"} />
+            <SummaryCard heading={"Total in"} title={"USD"} content={statistics ?statistics.total_balance_usd.toFixed(2):""} />
+            <SummaryCard heading={"Total in"} title={"CDF"} content={statistics ? statistics.total_balance_cdf.toLocaleString():""} />
             <SummaryCard heading={"Members"} icon={<HiOutlineUserGroup className={"card__icon"} />} content={"10"} />
         </div>
 
