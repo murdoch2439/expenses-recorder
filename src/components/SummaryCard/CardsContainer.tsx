e75fe95e25714838e1d96d68a361b608e6e802c7
mr2k <murdochkhallz@gmail.com>
2023-05-15 19:19:34 +0100

    Closing modal after adding successfully a contribution

--- src/components/SummaryCard/CardsContainer.tsx
+++ src/components/SummaryCard/CardsContainer.tsx
@@ -35,7 +35,7 @@ export const CardsContainer: FunctionComponent =()=>{
                 })
             }
 
-            {/*<Sumf.toLocaleString():""} />*/}
+            {/*<Sumf*/}
             {/*<SummaryC />*/}
         </div>
 
