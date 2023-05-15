8ea2a827259ae337a2b4b48f5deee393fb56a36f
mr2k <murdochkhallz@gmail.com>
2023-05-15 19:45:43 +0100

    Closing modal after adding successfully a contribution

--- src/components/dataTable/Table.tsx
+++ src/components/dataTable/Table.tsx
@@ -33,17 +33,7 @@ const columns: GridColDef[] = [
     },
 ];
 
-const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
+
 
 export const  Table : FunctionComponent=()=> {
     const {information:{contributions}} = useUserContext()
@@ -54,8 +44,7 @@ export const  Table : FunctionComponent=()=> {
                 contributions
                     ?
                     <DataGrid
-                        // rows={ rows}
-                        rows={contributions? contributions : rows}
+                        rows={contributions}
                         columns={columns}
                         getRowId={(row) => row._id}
                         initialState={{
