5103cc3d7ee2521d8e5fdda222a08ec5d56866cd
mr2k <murdochkhallz@gmail.com>
2023-05-19 15:26:55 +0100

    Adding generic select input component for other  select then members

---
+++ src/components/selectInput/StandardSelectInput.tsx
@@ -0,0 +1,47 @@
+import React, {FunctionComponent} from "react";
+import {Grid,Typography, TextField, FormControl, MenuItem,  } from '@mui/material';
+import {expenseType} from "../../utils/expenseType";
+
+type props = {
+    label?:string,
+    stuff?:any,
+    name?:any,
+    dataList:any
+}
+
+export const StandardSelectInput : FunctionComponent<props> =({ label, stuff, name, dataList})=>{
+    return(
+
+        <Grid item xs={12} sm={12} md={6}>
+            <FormControl fullWidth>
+                <TextField
+                    name={name}
+                    variant="outlined"
+                    select
+                    required
+                    label={label}
+                    onChange={stuff.handleChange}
+                    onBlur={stuff.handleBlur}
+                    value={stuff.values[name]}
+                    error={(stuff.touched[name] && stuff.errors[name]) && true}
+                    helperText={stuff.errors[name] && stuff.touched[name] && stuff.errors[name]}
+
+                >
+                    {
+                        dataList.map((option:any) => (
+                        <MenuItem key={option.value} value={option.value}>
+                            {option.label}
+                        </MenuItem>
+                    ))
+                    }
+
+
+
+
+                </TextField>
+            </FormControl>
+        </Grid>
+    )
+}
+
+
